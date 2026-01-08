f759481cb4876932d6cc2b6d1290587debb082f6
Suchitra Rajkumar <suchiraj0806@gmail.com>
2026-01-08 16:25:14 -0600

    Array problems

---
+++ products-of-array-except-self.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+        return res
